b6a650e213a20b4ae65b4942d56d69b6c76ca94b
Amr-Haitham <amrofficialacc2@gmail.com>
2024-09-17 14:51:39 +0300

    implemented comparable

--- lib/models/campus.dart
+++ lib/models/campus.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:collection/collection.dart';
 
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Campus {
+class Campus implements Comparable<Campus> {
   String id;
   String name;
   Campus({
@@ -42,9 +42,12 @@ class Campus {
       Campus.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() =>
-      'Campus(id: $id, name: $name)';
+  String toString() => 'Campus(id: $id, name: $name)';
+  @override
+  int compareTo(Campus other) {
+    return name.compareTo(other.name);
+  }
 
   @override
-  int get hashCode => id.hashCode  ^ name.hashCode;
+  int get hashCode => id.hashCode ^ name.hashCode;
 }
