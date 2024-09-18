5b99d64caa2cc07bff3cee43eda308040cd59ca4
Amr-Haitham <amrofficialacc2@gmail.com>
2024-09-18 15:16:38 +0300

    added to and from map of global workshop

--- lib/models/course/global/global_workshop.dart
+++ lib/models/course/global/global_workshop.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 // import 'package:roots_octopus/3_data_layer/models/static/quesion.dart';
 
+import 'dart:convert';
 import 'dart:typed_data';
+
+import 'package:collection/collection.dart';
 
 import 'question.dart';
 
@@ -13,7 +16,7 @@ class GlobalWorkshop {
   final String instructorNotesLink;
   final String instructorSlidesLink;
   final String imageLink;
-  List<String> studentsWhoAttendedIds = [];
+  final List<String> studentsWhoAttendedIds;
 
   GlobalWorkshop(
       {required this.id,
@@ -22,6 +25,70 @@ class GlobalWorkshop {
       required this.videoLink,
       required this.instructorNotesLink,
       required this.instructorSlidesLink,
-      required this.imageLink});
+      required this.imageLink,
+      required this.studentsWhoAttendedIds});
 
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'description': description,
+      'videoLink': videoLink,
+      'instructorNotesLink': instructorNotesLink,
+      'instructorSlidesLink': instructorSlidesLink,
+      'imageLink': imageLink,
+      'studentsWhoAttendedIds': studentsWhoAttendedIds,
+    };
+  }
+
+  factory GlobalWorkshop.fromMap(Map<String, dynamic> map) {
+    return GlobalWorkshop(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      description: map['description'] as String,
+      videoLink: map['videoLink'] as String,
+      instructorNotesLink: map['instructorNotesLink'] as String,
+      instructorSlidesLink: map['instructorSlidesLink'] as String,
+      imageLink: map['imageLink'] as String,
+      studentsWhoAttendedIds:
+          List<String>.from((map['studentsWhoAttendedIds'] as List<dynamic>)),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory GlobalWorkshop.fromJson(String source) =>
+      GlobalWorkshop.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'GlobalWorkshop(id: $id, name: $name, description: $description, videoLink: $videoLink, instructorNotesLink: $instructorNotesLink, instructorSlidesLink: $instructorSlidesLink, imageLink: $imageLink, studentsWhoAttendedIds: $studentsWhoAttendedIds)';
+  }
+
+  @override
+  bool operator ==(covariant GlobalWorkshop other) {
+    if (identical(this, other)) return true;
+    final listEquals = const DeepCollectionEquality().equals;
+
+    return other.id == id &&
+        other.name == name &&
+        other.description == description &&
+        other.videoLink == videoLink &&
+        other.instructorNotesLink == instructorNotesLink &&
+        other.instructorSlidesLink == instructorSlidesLink &&
+        other.imageLink == imageLink &&
+        listEquals(other.studentsWhoAttendedIds, studentsWhoAttendedIds);
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        description.hashCode ^
+        videoLink.hashCode ^
+        instructorNotesLink.hashCode ^
+        instructorSlidesLink.hashCode ^
+        imageLink.hashCode ^
+        studentsWhoAttendedIds.hashCode;
+  }
 }
--- lib/models/users/volunteer.dart
+++ lib/models/users/volunteer.dart
@@ -1,13 +1,13 @@
 import 'roots_user.dart';
 
-class Volunteer extends RootsUser {
-  Volunteer(
-      {required super.id,
-      required super.fullName,
-      required super.email,
-      required super.joinDate,
-      required super.phoneNumber,
-      required super.dob,
-      required super.homeAddress,
-      required super.gender});
-}
+// class Volunteer extends RootsUser {
+//   // Volunteer(
+//   //     {required super.id,
+//   //     required super.fullName,
+//   //     required super.email,
+//   //     required super.joinDate,
+//   //     required super.phoneNumber,
+//   //     required super.dob,
+//   //     required super.homeAddress,
+//   //     required super.gender});
+// }
