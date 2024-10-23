01c22e8c1808477d0d43a09b713a98e8af73708a
Amr-Haitham <amrofficialacc2@gmail.com>
2024-10-24 00:47:03 +0300

    added getExtention() to uploadedFileData

--- lib/models/helper_models/uploaded_file_data.dart
+++ lib/models/helper_models/uploaded_file_data.dart
@@ -1,33 +1,48 @@
-enum UploadedFileType{
-  image,
-  video,
-  document
-}
+enum UploadedFileType { image, video, document }
+
 class UploadedFileData {
   final String path;
   final String url;
   final UploadedFileType type;
-  UploadedFileData({
-    required this.path,
-    required this.url,
-    required this.type
-  });
+  UploadedFileData({required this.path, required this.url, required this.type});
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'path': path,
-      'url': url,
-      'type':type.index
-    };
+    return <String, dynamic>{'path': path, 'url': url, 'type': type.index};
   }
-
-
 
   factory UploadedFileData.fromMap(Map<String, dynamic> map) {
     return UploadedFileData(
-      path: map['path'] as String,
-      url: map['url'] as String,
-      type:UploadedFileType.values[map['type']]
-    );
+        path: map['path'] as String,
+        url: map['url'] as String,
+        type: UploadedFileType.values[map['type']]);
   }
+
+  getExtention() => path.split('.').last.toLowerCase();
+
+  //   static UploadedFileType getFileTypeFromPath() {
+  //   final extension = path.split('.').last.toLowerCase();
+
+  //   const imageExtensions = ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp'];
+  //   const videoExtensions = ['mp4', 'mov', 'avi', 'mkv', 'flv', 'wmv'];
+  //   const documentExtensions = [
+  //     'pdf',
+  //     'doc',
+  //     'docx',
+  //     'txt',
+  //     'xls',
+  //     'xlsx',
+  //     'ppt',
+  //     'pptx'
+  //   ];
+
+  //   if (imageExtensions.contains(extension)) {
+  //     return UploadedFileType.image;
+  //   } else if (videoExtensions.contains(extension)) {
+  //     return UploadedFileType.video;
+  //   } else if (documentExtensions.contains(extension)) {
+  //     return UploadedFileType.document;
+  //   } else {
+  //     throw Exception('Unsupported file type for extension: .$extension');
+  //   }
+  // }
 }
