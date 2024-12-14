ded102f0121c9a1ee37019012018ff3baf1311c6
Tanitolus <129631204+Tanitolus@users.noreply.github.com>
2024-12-14 18:51:54 +0000

    error page

--- lib/widgets/Error.dart
+++ lib/widgets/Error.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../app_constants/app_colors.dart';
+import '../theme/app_text_styles.dart';
+import '../utility-functions/image_utility_functions.dart';
+
 
 
 
 
 
 class ErrorPage extends StatelessWidget {
-  const ErrorPage({super.key});
+  const ErrorPage({super.key, this.message});
+  final String? message;
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            SizedBox(
-                height: 80,
-                child:
-                Image.asset("assets/Roots-Final-Logo-Transparent.png")),
-            const SizedBox(height: 30),
-            const Padding(
-              padding: EdgeInsets.all(17.0),
-              child: Text(
-                "There was an error. please double check the link or contact support at intelltanitox@gmail.com",
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const Spacer(),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+              height: 75,
+              child: Image(image: ImageUtils.getImage("assets/images/RootsIcons/Icons_PUZZLE (1).png"))),
+          const SizedBox(height: 15),
+          Text("Uh oh!", style: AppTextStyles.bigText.copyWith(color: AppColors.darkBlue, fontWeight: FontWeight.bold)),
+          Text(message ?? "Looks like something went wrong"),
+          // const SizedBox(height: 32),
+        ],
       ),
     );
   }
